c8125803802919e4a00e1c81c01c34cde1f1a6ee
Vaipa <cancanov47@gmail.com>
2026-03-19 17:52:08 +0300

    Скрипты меню без сво творения
    помогите я сижу рядом с чертями

---
+++ Assets/Pixel Adventure 1/СВЕРХ МОЛОДЁЖНЫЕ И СОВЕРМЕНЫЕ СКРИПТЫ/MenuLevel.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLevel : MonoBehaviour
+{
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadLevel_1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void LoadLevel_2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void LoadLevel_3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void LoadLevel_4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void LoadLevel_5()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void LoadLevel_6()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void LoadLevel_7()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void LoadLevel_8()
+    {
+        SceneManager.LoadScene(8);
+    }
+    public void LoadLevel_9()
+    {
+        SceneManager.LoadScene(9);
+    }
+}
